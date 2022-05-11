ee52728bf8a0c5c3e6ffe274b603789101fa7a6c
Matej Klemen <matej.klemen1337@gmail.com>
2022-05-11 17:19:52 +0200

    Add DataFrame loader, function for converting documents to examples, and torch Dataset loader

---
+++ data.py
@@ -0,0 +1,85 @@
+import ast
+import itertools
+from typing import Dict, Optional, Tuple, List
+
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+
+# TODO: There are more tags, but we currently ignore them (e.g., MFlag), i.e. encode them as "O"
+TAG2ID = {
+	"simple": {_tag: _i for _i, _tag in enumerate(["not_metaphor", "metaphor"])},
+	"independent": {_tag: _i for _i, _tag in enumerate(["O", "MRWd", "MRWi", "WIDLI"])},
+	"iob2": {_tag: _i for _i, _tag in enumerate(["O"] + list(map(lambda tup: "".join(tup),
+																 itertools.product(["B-", "I-"], ["MRWd", "MRWi", "WIDLI"]))))}
+}
+ID2TAG = {curr_scheme: {_i: _tag for _tag, _i in TAG2ID[curr_scheme].items()} for curr_scheme in TAG2ID}
+# This is used to mark labels that should not be taken into account in loss calculation
+LOSS_IGNORE_INDEX = -100
+
+
+class TransformersSeqDataset(Dataset):
+	def __init__(self, **kwargs):
+		self.valid_attrs = []
+		for attr, values in kwargs.items():
+			self.valid_attrs.append(attr)
+			setattr(self, attr, values)
+
+		assert len(self.valid_attrs) > 0
+
+	def __getitem__(self, item):
+		return {k: getattr(self, k)[item] for k in self.valid_attrs}
+
+	def __len__(self):
+		return len(getattr(self, self.valid_attrs[0]))
+
+
+def load_df(file_path) -> pd.DataFrame:
+	df = pd.read_csv(file_path, sep="\t")
+	if "sentence_words" in df.columns:
+		df["sentence_words"] = df["sentence_words"].apply(ast.literal_eval)
+	if "met_type" in df.columns:
+		df["met_type"] = df["met_type"].apply(ast.literal_eval)
+	if "met_frame" in df.columns:
+		df["met_frame"] = df["met_frame"].apply(ast.literal_eval)
+
+	return df
+
+
+def create_examples(df: pd.DataFrame, encoding_scheme: Dict[str, int],
+					history_prev_sents: int = 1,
+					fallback_label: Optional[str] = "O") -> Tuple[List, List]:
+	"""
+	:param df:
+	:param encoding_scheme: dict
+	:param history_prev_sents: Number of previous sentences to take as context. Labels for this context are masked out
+	so that the model does not predict token labels multiple times.
+	:param fallback_label: Default negative label; used for unrecognized ("other") labels
+	"""
+	assert history_prev_sents >= 0
+	examples_input = []
+	examples_labels = []
+
+	for curr_doc, curr_df in df.groupby("document_name"):
+		sorted_order = np.argsort(curr_df["idx_sentence_glob"].values)
+		curr_df_inorder = curr_df.iloc[sorted_order]
+
+		tokens, labels = [], []
+		for idx_row in range(len(sorted_order)):
+			curr_row = curr_df_inorder.iloc[idx_row]
+			tokens.append(curr_row["sentence_words"])
+			labels.append(curr_row["met_type"])
+
+			history_tokens = list(itertools.chain(*tokens[(-1 - history_prev_sents): -1]))
+			curr_tokens = tokens[-1]
+
+			curr_ex_tokens = history_tokens + curr_tokens
+			curr_ex_labels = [-100] * len(history_tokens) + \
+							 list(map(lambda str_lbl: encoding_scheme.get(str_lbl, encoding_scheme[fallback_label]), labels[-1]))
+
+			assert len(curr_ex_tokens) == len(curr_ex_labels)
+
+			examples_input.append(curr_ex_tokens)
+			examples_labels.append(curr_ex_labels)
+
+	return examples_input, examples_labels
