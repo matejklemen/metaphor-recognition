19a8443edfc8d142f20850aa4ae102084e5aec85
Matej Klemen <matej.klemen1337@gmail.com>
2022-05-29 19:33:04 +0200

    Minor fix in metric computation: num_eval_labels now points to num pos. labels; previously pointed to num of pos+neg labels

--- base.py
+++ base.py
@@ -171,7 +171,7 @@ class MetaphorController:
 
 	def compute_metrics(self, true_labels, predicted_labels):
 		metrics = {}
-		pos_labels = list(range(1, self.num_eval_labels))  # all but the fallback label
+		pos_labels = list(range(1, 1 + self.num_eval_labels))  # all but the fallback label (eval on pos labels)
 		macro_p, macro_r, macro_f1 = 0.0, 0.0, 0.0
 		for curr_label in pos_labels:
 			curr_label_str = ID2TAG[self.sec_label_scheme][curr_label]
