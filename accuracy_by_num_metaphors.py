5829177a1beddcd3ff371f9b74f0e4391968821a
Matej Klemen <matej.klemen1337@gmail.com>
2022-11-22 13:24:21 +0100

    Convert accuracy_by_num_metaphors to be focused on a single system, add token-level recall option

--- accuracy_by_num_metaphors.py
+++ accuracy_by_num_metaphors.py
@@ -1,83 +1,79 @@
-import argparse
-import os
 from collections import Counter
 
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 from data import load_df
 
 if __name__ == "__main__":
-	model_results_dir = [
-		"RESULTS/SENTENCE/komet-sent-csebert-2e-5-binary3-history0-optthresh"
+	model_results_paths = [
+		"RESULTS/WORD/komet-csebert-2e-5-binary3-history0-optthresh/komet-fold0-csebert-2e-5-binary3-history0-optthresh/predict-test/test_results.tsv",
+		"RESULTS/WORD/komet-csebert-2e-5-binary3-history0-optthresh/komet-fold1-csebert-2e-5-binary3-history0-optthresh/predict-test/test_results.tsv",
+		"RESULTS/WORD/komet-csebert-2e-5-binary3-history0-optthresh/komet-fold2-csebert-2e-5-binary3-history0-optthresh/predict-test/test_results.tsv",
+		"RESULTS/WORD/komet-csebert-2e-5-binary3-history0-optthresh/komet-fold3-csebert-2e-5-binary3-history0-optthresh/predict-test/test_results.tsv",
+		"RESULTS/WORD/komet-csebert-2e-5-binary3-history0-optthresh/komet-fold4-csebert-2e-5-binary3-history0-optthresh/predict-test/test_results.tsv",
 	]
-	short_names = [
-		"csebert"
-	]
-	systems_indexer = np.arange(len(model_results_dir))
-	cmap = plt.cm.get_cmap("tab10")
-	assert len(model_results_dir) <= 10, "If you need more than 10 colors, remove assert and change cmap to 'tab20'"
-	system_colors = [cmap(i) for i in range(len(model_results_dir))]
 
-	file_name = "test_results.tsv"
+	df = pd.concat([load_df(curr_path) for curr_path in model_results_paths], axis=0).reset_index(drop=True)
+	df["num_met_words"] = df["met_type"].apply(lambda _mets: sum([len(met_info["word_indices"]) for met_info in _mets]))
+	df["prop_met_words"] = df["num_met_words"] / df["sentence_words"].apply(lambda _words: len(_words))
 
-	num_mets_to_stats = {}  # 0 -> {system1: ..., system2: ...}, 1 -> {...}
-	num_mets_to_total = {}
+	# These are defined by feel, making sure that each bin has enough samples to infer something from it
+	def _discretize(_p):
+		if _p == 0.0:
+			return "0.0"
+		elif 0.0 < _p <= 0.1:
+			return "(0.0, 0.1]"
+		elif 0.1 < _p <= 0.2:
+			return "(0.1, 0.2]"
+		else:
+			return "(0.2, 1]"
+	df["prop_met_words"] = df["prop_met_words"].apply(_discretize)
 
-	for model_dir, model_name in zip(model_results_dir, short_names):
-		print(f"'{model_name}'")
-		results_dir = [os.path.join(model_dir, dirname) for dirname in os.listdir(model_dir)
-					   if os.path.isdir(os.path.join(model_dir, dirname))]
-		results_dir = sorted(results_dir)
+	def token_correctness(_preds, _true):
+		return Counter([_p == _t for _p, _t in zip(_preds, _true) if _t not in {"O", 0, "not_metaphor"}])  # RECALL!
 
-		num_metaphors_correctness = {}
-		for curr_dir in results_dir:
-			file_path = os.path.join(curr_dir, file_name)
-			df = load_df(file_path)
+	def sentence_correctness(_preds, _true):
+		return Counter([_preds == _true])  # ACCURACY
 
-			df["num_metaphors"] = df["met_type"].apply(lambda _mets: len(_mets))
-			for num_mets, curr_group in df.groupby("num_metaphors"):
-				existing_count = num_metaphors_correctness.get(num_mets, Counter())
-				existing_count += Counter((curr_group["preds_transformed"] == curr_group["true_transformed"]).tolist())
-				num_metaphors_correctness[num_mets] = existing_count
+	plot_desc = None
+	groups = ["0.0", "(0.0, 0.1]", "(0.1, 0.2]", "(0.2, 1]"]
+	correctness_stats = []
+	total_counted = []
+	for curr_prop in groups:
+		curr_group = df.loc[df["prop_met_words"] == curr_prop]
 
-		for num, correctness in num_metaphors_correctness.items():
-			existing_stats = num_mets_to_stats.get(num, {})
-			existing_stats[model_name] = correctness[True] / (correctness[True] + correctness[False])
-			num_mets_to_stats[num] = existing_stats
+		print(f"{curr_prop}: {curr_group.shape[0]} examples")
+		group_correctness = Counter()
+		for idx_ex in range(curr_group.shape[0]):
+			curr_ex = curr_group.iloc[idx_ex]
+			curr_preds, curr_true = curr_ex["preds_transformed"], curr_ex["true_transformed"]
 
-			num_mets_to_total[num] = (correctness[True] + correctness[False])
+			if isinstance(curr_preds, list) and isinstance(curr_true, list):
+				plot_desc = "Token-level metaphor recall"
+				group_correctness += token_correctness(curr_preds, curr_true)
+			elif isinstance(curr_preds, str) and isinstance(curr_true, str):
+				plot_desc = "Sentence-level accuracy"
+				group_correctness += sentence_correctness(curr_preds, curr_true)
+			else:
+				raise ValueError(f"Encountered unexpected combination of types: {type(curr_preds)} and {type(curr_true)}")
 
-	NUM_UNIQ_PLOTS = len(num_mets_to_stats.keys())
-	SUBPLOTS_PER_ROW = 4
-	num_vis_rows = (NUM_UNIQ_PLOTS + SUBPLOTS_PER_ROW - 1) // SUBPLOTS_PER_ROW
+		denom = group_correctness[True] + group_correctness[False]
+		correctness_stats.append(group_correctness[True] / max(1, denom))
+		total_counted.append(denom)
 
-	plt.figure(figsize=(20, 35))
-	plt.suptitle("Sentence-level accuracy by number of annotated metaphors in sentence")
-	for _i, (num_met, accuracies) in enumerate(
-			sorted(num_mets_to_stats.items(), key=lambda tup: tup[0]), start=1
-	):
-		curr_total = num_mets_to_total[num_met]
-		sys_names = list(map(lambda tup: tup[0], accuracies.items()))
-		sys_accs = list(map(lambda tup: tup[1], accuracies.items()))
+	indexer = np.arange(len(groups))
+	plt.figure(figsize=(7, 5))
+	plt.title(f"{plot_desc} by number of met. words in a sentence")
+	plt.barh(indexer, correctness_stats)
+	plt.yticks(indexer, groups)
+	plt.ylabel("Proportion of met. words in a sentence", rotation="vertical")
+	plt.xlabel("Accuracy")
 
-		sort_indices = np.argsort(-np.array(sys_accs))
-		plt.subplot(num_vis_rows, SUBPLOTS_PER_ROW, _i)
-		plt.title(f"num_met={num_met} (N={curr_total})", pad=10, fontsize=12)
-		plt.barh(systems_indexer, [sys_accs[_i] for _i in sort_indices], height=0.1,
-				 color=[system_colors[_i] for _i in sort_indices])
-		plt.xlim([0.0, 1.0 + 0.05])
-		plt.xticks(np.arange(0.0, 1.0 + 0.05, 0.1))
-		plt.yticks(systems_indexer, [sys_names[_i] for _i in sort_indices])
-
-		for i in range(len(systems_indexer)):
-			curr_acc = sys_accs[sort_indices[i]]
-			plt.text(curr_acc, i, f"{curr_acc:.3f}")
-
-		print(f"num_mets = {num_met}")
-		print(sys_names)
-		print(sys_accs)
+	for _i in indexer:
+		plt.text(correctness_stats[_i], _i - 0.05, f"{correctness_stats[_i]:.3f} (N={total_counted[_i]})")
 
 	plt.tight_layout()
-	plt.subplots_adjust(top=0.9, hspace=0.5)
 	plt.show()
+
